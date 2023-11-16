eb7494336d4b8d8fb9fe25ec7aa82cc7e88b673d
vitorcolazelli <vitorcolazelli@gmail.com>
2023-11-16 14:28:49 -0300

    Adicionar arquivos de projeto.

---
+++ Ecommerce.Services.CouponAPI/Data/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Ecommerce.Services.CouponAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Ecommerce.Services.CouponAPI.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Coupon> Coupons { get; set; }
+    }
+}
