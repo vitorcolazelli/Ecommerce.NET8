fe6ec797f4f5def036a2c2962587f57243e2fa8e
vitorcolazelli <vitorcolazelli@gmail.com>
2023-11-18 14:53:11 -0300

    Criando projeto do frontend MVC e criando models de request e response

---
+++ Ecommerce.Web/Models/RequestDto.cs
@@ -0,0 +1,12 @@
+﻿using static Ecommerce.Web.Utility.SD;
+
+namespace Ecommerce.Web.Models
+{
+    public class RequestDto
+    {
+        public ApiType ApiType { get; set; } = ApiType.GET;
+        public string Url { get; set; }
+        public object Data { get; set; }
+        public object AccessToken { get; set; }
+    }
+}
---
+++ Ecommerce.Web/Utility/SD.cs
@@ -0,0 +1,13 @@
+﻿namespace Ecommerce.Web.Utility
+{
+    public class SD
+    {
+        public enum ApiType
+        {
+            GET,
+            POST, 
+            PUT, 
+            DELETE
+        }
+    }
+}
