470fa12fcac8adced539d4ce1302a36818ea14c5
vitorcolazelli <vitorcolazelli@gmail.com>
2024-02-19 19:51:36 -0300

    Criando projeto base do carrinho

---
+++ Eccomerce.Services.ShoppingCartAPI/Data/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Eccomerce.Services.ShoppingCartAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Eccomerce.Services.ShoppingCartAPI.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<CartHeader> CartHeader { get; set; }
+        public DbSet<CartDetails> CartDetails { get; set; }
+    }
+}
---
+++ Eccomerce.Services.ShoppingCartAPI/MappingConfig.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using Eccomerce.Services.ShoppingCartAPI.Models;
+using Eccomerce.Services.ShoppingCartAPI.Models.Dto;
+
+namespace Eccomerce.Services.ShoppingCartAPI
+{
+    public class MappingConfig
+    {
+        public static MapperConfiguration RegisterMaps()
+        {
+            var mappingConfig = new MapperConfiguration(config =>
+            {
+                config.CreateMap<CartHeader, CartHeaderDto>().ReverseMap();
+                config.CreateMap<CartDetails, CartDetailsDto>().ReverseMap();
+            });
+
+            return mappingConfig;
+        }
+    }
+}
